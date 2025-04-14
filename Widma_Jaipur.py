0d7a0eb5332ffd0259816573e4e5f499992e989f
ManishaDayaala <Manishadayaala@gmail.com>
2025-04-14 18:30:03 +0530

    Update Widma_Jaipur.py

--- Widma_Jaipur.py
+++ Widma_Jaipur.py
@@ -26,7 +26,7 @@ def set_random_seed(seed_value=42):
     tf.random.set_seed(seed_value)
 
 # Define the main folder path
-MAINFOLDER = r"D:\APPdata_WIDMA_Jaipur"
+MAINFOLDER = r"./APPdata_WIDMA_Jaipur"
 
 # Create other paths relative to the main folder
 training_file_path = os.path.join(MAINFOLDER, "Training", "Training_WIDMA.xlsx")  # FIXED TRAINING DATA
